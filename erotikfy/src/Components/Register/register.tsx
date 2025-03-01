9e4ddb14ad7de3c623f1ffc67b13a583851f2373
Rkz Jacob <jacobponce64@gmail.com>
2025-03-01 17:49:57 -0300

    a
    a

--- erotikfy/src/Components/Register/register.tsx
+++ erotikfy/src/Components/Register/register.tsx
@@ -4,11 +4,12 @@ import {  SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { InputForm } from './inputsForm';
 import background from '../../assets/register_realike.jpg'
+import { useMutation } from '@apollo/client';
+import { REGISTER_MUTATION_USERNORMAL } from '../../Mutations/mutations';
 
 
 const schema = z.object({
     nombre_usuario: z.string().min(1,"El nombre se usuario es obligatorio"),
-    email: z.string().email("Correo invalido").min(1,"El correo es obligatorio"),
     contrasena: z.string().min(6,"La contraseña debe contener almenos 6 caracteres"),
     confirmPassword: z.string().min(6,"La confirmacion debe tener almenos 6 caracteres")
 }).refine(data=>data.contrasena === data.confirmPassword,{
@@ -19,29 +20,28 @@ const schema = z.object({
 type FormValues = z.infer<typeof schema>;
 
 export const Registrar = () =>{
-    // const [createUser,{loading ,error}] = useMutation(REGISTER_MUTATION);
+    const [createUser,{loading ,error}] = useMutation(REGISTER_MUTATION_USERNORMAL);
     const {control,handleSubmit,formState:{errors}} = useForm<FormValues>({
         resolver:zodResolver(schema),
         mode:"onBlur",
         defaultValues:{
             nombre_usuario:"",
-            email:"",
             contrasena:"",
             confirmPassword:""
         }
     });
 
-    // const onSubmit: SubmitHandler<FormValues> = async (formData) =>{
-    //     try {
-    //         const {data}= await createUser({
-    //             variables: {nombre_usuario: formData.nombre_usuario, email:formData.email,contrasena:formData.confirmPassword },
-    //           });
-    //         console.log("usuario registrado",data)
+    const onSubmit: SubmitHandler<FormValues> = async (formData) =>{
+        try {
+            const {data}= await createUser({
+                variables: {username: formData.nombre_usuario,contrasena:formData.confirmPassword },
+              });
+            console.log("usuario registrado",data)
             
-    //     } catch (error) {
-    //         console.log("usuario no registrado",error)
-    //     }
-    // }
+        } catch (error) {
+            console.log("usuario no registrado",error)
+        }
+    }
     return (
         <section className="register">
             <div className="content-register">
@@ -54,7 +54,7 @@ export const Registrar = () =>{
                 <div className='register_container-title'>
                     <h2>REGISTRAR CUENTA</h2>
                 </div>
-                    <form className="register__form" /*onSubmit={handleSubmit(onSubmit)}  */>
+                    <form className="register__form" onSubmit={handleSubmit(onSubmit)}  >
                         <div>
                             <i className="fa fa-user"></i>
                             <InputForm name='nombre_usuario' control={control} label='Nombre de usuario' type='string' error={errors.nombre_usuario}/>
@@ -67,7 +67,7 @@ export const Registrar = () =>{
                             <i className="fa fa-lock"></i>
                             <InputForm name='confirmPassword' control={control} label='Confirmar contraseña' type='password' error={errors.confirmPassword}/>
                         </div>
-                        <button className='register__button' type='submit'  >
+                        <button className='register__button' type='submit'  disabled={loading}>
                             Registrarse
                         </button>
                     </form> 
--- erotikfy/src/Mutations/mutations.tsx
+++ erotikfy/src/Mutations/mutations.tsx
@@ -8,7 +8,7 @@ export const LOGIN_MUTATION = gql`
 `
 
 export const REGISTER_MUTATION_USERNORMAL = gql`
-    mutation createUser($username: String!, $email: String!, $contrasena: String!) {
-        CREATE_USER(username: $username,contrasena: $contrasena,email:"")
+    mutation createUser($username: String!, $contrasena: String!) {
+        CREATE_USER_NORMAL(username: $username,contrasena: $contrasena)
     }
     `;
