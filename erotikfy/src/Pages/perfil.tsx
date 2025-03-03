40c9c1c669a7c3a4a9588f29db3b6fcfeb8c4ea7
Pxlxr <Brayan.pardo.sk8@gmail.com>
2025-03-03 16:01:43 -0300

    Update Perfil.tsx

--- erotikfy/src/Pages/perfil.tsx
+++ erotikfy/src/Pages/perfil.tsx
@@ -1,5 +1,4 @@
 import { PerfilList } from "../Components/perfil/perfil";
-import { PerfilUser } from "../Components/PerfilUser/perfiluser";
 import { Sidebar } from "../Components/Sidebar/sidebar";
 
 export function Perfil () {
