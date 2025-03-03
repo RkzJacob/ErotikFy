50c5dbea40580e6138585d7442c283e149612c1e
Pxlxr <Brayan.pardo.sk8@gmail.com>
2025-03-03 19:59:47 -0300

    ads

--- erotikfy/src/Components/ads/ads.tsx
+++ erotikfy/src/Components/ads/ads.tsx
@@ -1,23 +1,19 @@
-
 import "./ads.css"; // Asegúrate de que el CSS está en la misma carpeta
-import ads from '../../LocalImagen/ads.mp4' 
+import ads from '../../LocalImagen/ads.mp4';
+
 export const Ads = () => {
   return (
-    <div className="container-ads">
-      {/* Contenedor del video */}
+  <div className="container-ads">
+    <div className="video-wrapper">
       <div className="video-container">
-        <video className="video-player" controls>
-          <source src= {ads} type="video/mp4" />
-        </video>
-      </div>
-
-      {/* Contenedor del mensaje */}
-      <div className="message-container">
-        <p className="message-text">
-          ¡Paga tu suscripción!
-        </p>
+        <video className="video-player" src={ads} controls></video>
       </div>
     </div>
+
+    <div className="neon-sign">
+      <p>¡Paga tu suscripción!</p>
+    </div>
+  </div>
   );
 };
 
